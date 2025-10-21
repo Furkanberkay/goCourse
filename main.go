683a603303e983ddc7b83be6da3a89db0802a003
FurkanBerkay <furkanberkayozcan@hotmail.com>
2025-10-21 14:20:29 +0300

    initial commit: add main.go with basic variable declarations and print statements

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var metin string = "selam"
+	fmt.Println(metin)
+
+	var kdv int = 10
+
+	fmt.Print(kdv)
+
+	var vergi float32 = 10.2
+
+	fmt.Println(vergi)
+
+	kdv2 := 25.2
+	fmt.Printf("veri tipi :  %T : ", kdv2)
+}
